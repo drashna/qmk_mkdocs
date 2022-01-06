331d8cfa7259a9146d4bf2077c2e776d2159e09d
Drashna Jael're <drashna@live.com>
2022-01-05 18:20:33 -0800

    See about fixing mike

--- utils/symlink_translation.sh
+++ utils/symlink_translation.sh
@@ -15,13 +15,13 @@ translation=$1
 cd "$(dirname "${BASH_SOURCE[0]}")"/../docs/en
 
 for file in $(find docs -type f); do
-	if ! [ -e ../${translation}/${file} ]; then
+	if ! [ -e ../${file}/${translation} ]; then
 		# Ugly hack, but ChangeLog is the only subdir we have...
 		if echo $file | grep -q '/.*/'; then
-			mkdir -p ../$(dirname ${translation}/${file})
-			ln -s ../../../en/${file} ../${translation}/${file}
+			mkdir -p ../$(dirname ${file}/${translation})
+			ln -s ../../../en/${translation} ../${file}/${translation}/
 		else
-			ln -s ../../en/${file} ../${translation}/${file}
+			ln -s ../../en/${translation} ../${file}/${translation}/
 		fi
 	fi
 done
